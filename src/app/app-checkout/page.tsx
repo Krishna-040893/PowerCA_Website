e9fe66c9fc24401d2fdc53458773de6a55af4de7
tbstechudt-dotcom <tbstechudt@gmail.com>
2025-12-20 12:26:29 +0530

    Update page.tsx

--- src/app/app-checkout/page.tsx
+++ src/app/app-checkout/page.tsx
@@ -375,7 +375,7 @@ function AppCheckoutContent() {
                   ) : (
                     <>
                       <Shield className="w-5 h-5 mr-2" />
-                      Place order
+                      Place Order
                     </>
                   )}
                 </Button>
